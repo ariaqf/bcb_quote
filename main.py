73ec053f7882e2e6e9de8510b46d9b43b1be4573
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-10-03 13:13:38 -0300

    main and input checking

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+import datetime
+
+def is_valid_date(date):
+    retr = False
+    if len(date) != 8:
+            print('Date is not in valid format, valid format is "YYYYMMDD".')
+    else:
+        try:
+            days = int(date[6:8])
+            months = int(date[4:6])
+            year = int(date[0:4])
+            asked_day = datetime.date(year,months,days)
+            today = datetime.date.today()
+            if (today < asked_day):
+                print('Date is not valid, please don\'t use future dates.')
+            else:
+                retr = True
+        except:
+            print('Date is not valid, valid format is "YYYYMMDD". Please use only numbers.')
+    return retr
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('Insufficient Parameters. You need to supply a date.')
+    if len(sys.argv) >= 2:
+        date = sys.argv[1]
+        if(is_valid_date(date)):
+            quote = get_quote_for_date(date)
+        
+        
+
