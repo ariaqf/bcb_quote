c131f73f2838b88ffa5529adada35e2b0e56f15d
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-10-03 17:50:59 -0300

    Change to enable showing code

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from input_checker import check_args
 from exceptions import NoQuoteForDateException
 from quote import get_best_quote_by_date
 
-def print_best_quote_for_date(date):
+def print_best_quote_for_date(date, show_code):
     try:
         best_quote = get_best_quote_by_date(date)
-        print(best_quote['symbol'] +', ' + best_quote['countries'] + ', %.8f' % best_quote['value'])
+        if show_code:
+            print(best_quote['code']+', '+best_quote['symbol'] +', ' + best_quote['countries'] + ', %.8f' % best_quote['value'])
+        else:
+            print(best_quote['symbol'] +', ' + best_quote['countries'] + ', %.8f' % best_quote['value'])
     except NoQuoteForDateException as e:
         print('x')
     except Exception as e:
@@ -15,5 +18,8 @@ def print_best_quote_for_date(date):
 
 if __name__ == '__main__':
     if(check_args(sys.argv)):
-        print_best_quote_for_date(date)
+        if(len(sys.argv) > 2):
+            print_best_quote_for_date(sys.argv[1], True)
+        else:
+            print_best_quote_for_date(sys.argv[1], False)
         
