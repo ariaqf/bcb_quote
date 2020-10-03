cc2fce6648b082a5b32bad447dbafa2b10ce8cec
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-10-03 17:14:37 -0300

    Cahnge printing

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from quote import get_best_quote_by_date
 def print_best_quote_for_date(date):
     try:
         best_quote = get_best_quote_by_date(date)
+        print(best_quote['symbol'] +', ' + best_quote['countries'] + ', ' + best_quote['value'])
     except NoQuoteForDateException as e:
         print('x')
     except Exception as e:
